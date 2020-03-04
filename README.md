9ae1b061b0f8bf09bbcca7cb2769efe7fc9b105d
Abdelrahman Mohamed <mrabdelrahman10@gmail.com>
2020-03-04 06:22:45 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# MySQL Cron Backup
+
+Run schedule to create full backup for mysql database and save it on server
+
+# Steps to install
+ 1. Rename **.env.example** to **.env** and update values
+ 2. Install packages via terminal **npm install**
+
+# Run via CLI
+```
+node index.js
+```
+
+## Anyone welcome to contact me anytime
+### My Website :- [mrabdelrahman10.com](http://bit.ly/WebMr10)
+### My Linkedin :- [linkedin.com/in/mrabdelrahman10](http://bit.ly/LiMr10)
---
+++ index.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+const mysqldump = require('mysqldump');
+
+const date = new Date();
+const file_name = `${date.getFullYear()}_${date.getMonth()}_${date.getDay()}_${date.getHours()}_${date.getMinutes()}_${date.getSeconds()}`;
+const backup_file = `./backups/${file_name}.sql.gz`;
+
+(async () => {
+    let is_running = false;
+    setInterval(async () => {
+        if (is_running) {
+            console.log('[task]', new Date(), 'another task is already running');
+            return;
+        }
+        console.log('[task]', new Date(), 'Task starts to run');
+        is_running = true;
+
+        // dump the result straight to a compressed file
+        await mysqldump({
+            connection: {
+                host: process.env.MYSQL_HOST,
+                user: process.env.MYSQL_USER,
+                password: process.env.MYSQL_PASSWORD,
+                database: process.env.MYSQL_DATABASE,
+            },
+            dumpToFile: backup_file,
+            compressFile: true,
+        });
+
+        console.log('[task]', new Date(), 'Backup file is:-', backup_file);
+
+        is_running = false;
+        console.log('[task]', new Date(), 'Task is finished');
+    }, parseInt(process.env.RUNNING_EVERY || 1) * 1000 * 60 * 60)
+
+})();
